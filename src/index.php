03effebe00f6fc900559520b76a7a3e62331250b
Raziel Rodrigues <razielx3@live.com>
2022-08-28 16:38:47 -0300

    feat: adicionando liskov

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'SingleResposability.php';
+echo '<hr>';
+require_once 'OpenClose.php';
+echo '<hr>';
+require_once 'OpenClose2.php';
+echo '<hr>';
+require_once 'LiskovSubstitution.php';
+echo '<hr>';
+require_once 'LiskovSubstitution2.php';
